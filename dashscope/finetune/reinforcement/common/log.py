c89454fe97e6bc847a493050675d8ca7d1d8ecca
dashscope-sdk <random_guy1231@outlook.com>
2026-05-13 11:08:06 +0800

    apt: fix sdk/cli

---
+++ dashscope/finetune/reinforcement/common/log.py
@@ -0,0 +1,33 @@
+import logging
+import sys
+
+from dashscope.finetune.reinforcement.common.constants import (
+    LOGGER_NAME,
+    LOG_LEVEL,
+)
+
+
+def setup_logger():
+    """Initialize and configure the logger with value masking."""
+    logger = logging.getLogger(LOGGER_NAME)
+    level_map = {
+        "DEBUG": logging.DEBUG,
+        "INFO": logging.INFO,
+        "WARNING": logging.WARNING,
+        "ERROR": logging.ERROR,
+        "CRITICAL": logging.CRITICAL,
+    }
+    logger.setLevel(level_map.get(LOG_LEVEL, logging.INFO))
+
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    logger.propagate = False
+    return logger
+
+
+logger = setup_logger()
---
+++ dashscope/finetune/reinforcement/component/processor/__init__.py
@@ -0,0 +1,19 @@
+from dashscope.finetune.reinforcement.component.processor.abstract_group_reward_processor import (
+    AbstractGroupRewardProcessor,
+)
+from dashscope.finetune.reinforcement.component.processor.abstract_processor import (
+    AbstractProcessor,
+)
+from dashscope.finetune.reinforcement.component.processor.abstract_reward_processor import (
+    AbstractRewardProcessor,
+)
+from dashscope.finetune.reinforcement.component.processor.abstract_rollout_processor import (
+    AbstractRolloutProcessor,
+)
+
+__all__ = [
+    "AbstractProcessor",
+    "AbstractRewardProcessor",
+    "AbstractRolloutProcessor",
+    "AbstractGroupRewardProcessor",
+]
